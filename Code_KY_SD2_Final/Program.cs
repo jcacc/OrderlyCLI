5240db64c64db098d4a1eabd1c5cbbf13a7d0d43
jcacc <joe@accardi.xyz>
2023-03-18 08:53:25 -0400

    adding sample data, program class

--- Code_KY_SD2_Final/Program.cs
+++ Code_KY_SD2_Final/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Code_KY_SD2_Final;
+using Serilog;
+using System.Text.RegularExpressions;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        string inputCsvFile = "C:\\tmp\\csharp_projects\\Code_KY_SD2_Final\\Code_KY_SD2_Final\\input.csv";
+        string outputCsvFile1 = "C:\\tmp\\csharp_projects\\Code_KY_SD2_Final\\Code_KY_SD2_Final\\output1.csv";
+        string outputCsvFile2 = "C:\\tmp\\csharp_projects\\Code_KY_SD2_Final\\Code_KY_SD2_Final\\output2.csv";
+
+        var processor = new OrderProcessor<Order>(inputCsvFile, outputCsvFile1, outputCsvFile2, order => Regex.IsMatch(order.FulfillmentType, @"^Location1$"));
+        processor.Process();
+
+        Log.Information("Order processing complete.");
+        Log.CloseAndFlush();
+    }
+}
